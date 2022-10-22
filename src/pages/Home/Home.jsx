ff8904f4c1d578e59019e27cf6664e502f61714e
maria-khabian <mariakhabian@gmail.com>
2022-10-22 17:40:33 +0200

    translate1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,57 +1,60 @@
 import React from 'react';
 // import AppContext from '../../AppContext';
 
-import Card from "../../components/Card/Card";
+import Card from '../../components/Card/Card';
 import close from './../../img/close.svg';
 import search from './../../img/search.svg';
 
 function Home({
-    items,
-    searchValue,
-    onChangeSearchInpute,
-    onClearInput,
-    onAddToCart,
-    onAddToFavorites,
-    isLoading }) {
+  items,
+  searchValue,
+  onChangeSearchInpute,
+  onClearInput,
+  onAddToCart,
+  onAddToFavorites,
+  isLoading,
+}) {
+  const renderItems = () => {
+    const filtredItems = items.filter((item) =>
+      item.title.toLowerCase().includes(searchValue.toLowerCase()),
+    );
+    //debugger
+    return isLoading
+      ? Array(8).fill(<Card loading={isLoading} />)
+      : filtredItems.map((item, index) => (
+          <Card
+            key={index}
+            onFavorite={(obj) => onAddToFavorites(obj)}
+            onPlus={(obj) => onAddToCart(obj)}
+            loading={isLoading}
+            {...item}
+          />
+        ));
+  };
 
-        
-    const renderItems = () => {
-        const filtredItems = items.filter(item =>
-            item.title.toLowerCase().includes(searchValue.toLowerCase())
-        )
-        //debugger
-        return (
-            isLoading ? Array(8).fill(<Card loading={isLoading} />) : filtredItems
-                .map((item, index) => (
-                    <Card
-                        key={index}
-                        onFavorite={(obj) => onAddToFavorites(obj)}
-                        onPlus={(obj) => onAddToCart(obj)}
-                        loading={isLoading}
-                        { ...item } />
-                ))
-        )
-    }
+  return (
+    <>
+      <div className="mainHeader">
+        <h1>{searchValue ? `Searching: ${searchValue}` : 'All sneakers'}</h1>
+        <div className="searchBlock">
+          <img width={14.25} height={14.25} src={search} alt="search" />
+          <input onChange={onChangeSearchInpute} value={searchValue} placeholder="Поиск..." />
+          {searchValue ? (
+            <img
+              onClick={onClearInput}
+              className="close"
+              width={21}
+              height={21}
+              src={close}
+              alt="close"
+            />
+          ) : null}
+        </div>
+      </div>
 
-    return (
-        <>
-            <div className='mainHeader'>
-
-                <h1>{searchValue ? `Идёт поиск: ${searchValue}` : 'Все кроссовки'}</h1>
-                <div className='searchBlock'>
-                    <img width={14.25} height={14.25} src={search} alt='search' />
-                    <input onChange={onChangeSearchInpute} value={searchValue} placeholder='Поиск...' />
-                    {searchValue
-                        ? <img onClick={onClearInput} className='close' width={21} height={21} src={close} alt='close' />
-                        : null}
-                </div>
-            </div>
-
-            <div className='sneakersCard'>
-                {renderItems()}
-            </div>
-        </>
-    )
+      <div className="sneakersCard">{renderItems()}</div>
+    </>
+  );
 }
 
 export default Home;
